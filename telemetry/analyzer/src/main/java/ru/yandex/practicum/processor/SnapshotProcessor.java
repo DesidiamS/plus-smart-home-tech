d5e0e2dc990322620f2d929fffa04d9078d347e6
desidiam <desidiam@gmail.com>
2025-07-17 19:39:46 +0300

    Исправления в analyzer

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/processor/SnapshotProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/processor/SnapshotProcessor.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.processor;
 
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
-import ru.practicum.deserializer.HubEventAvroDeserializer;
 import lombok.RequiredArgsConstructor;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -10,6 +9,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.practicum.deserializer.SnapshotAvroDeserializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorSnapshotAvro;
 import ru.yandex.practicum.service.SnapshotService;
 
@@ -28,7 +28,7 @@ public class SnapshotProcessor implements Runnable {
 
     @Override
     public void run() {
-        try (KafkaConsumer<String, SpecificRecordBase > consumer = new KafkaConsumer<>(getConsumerProperties())) {
+        try (KafkaConsumer<String, SpecificRecordBase> consumer = new KafkaConsumer<>(getConsumerProperties())) {
             consumer.subscribe(Collections.singleton(topic));
 
             Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
@@ -52,9 +52,9 @@ public class SnapshotProcessor implements Runnable {
         Properties properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, HubEventAvroDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SnapshotAvroDeserializer.class);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "snapshot.group.id");
-        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, "SnapshotConsumer");
+        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, "SnapshotsConsumer");
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         return properties;
