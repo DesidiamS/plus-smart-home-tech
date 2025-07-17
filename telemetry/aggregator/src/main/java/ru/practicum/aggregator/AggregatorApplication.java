c261a35afc054ca8c1d546d2dd51679949837eee
desidiam <desidiam@gmail.com>
2025-07-17 17:19:05 +0300

    Изменен агрегатор

--- telemetry/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApplication.java
+++ telemetry/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApplication.java
@@ -3,12 +3,15 @@ package ru.practicum.aggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AggregatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AggregatorApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(AggregatorApplication.class, args);
+        KafkaSensorSnapshot kafkaSensorSnapshot = context.getBean(KafkaSensorSnapshot.class);
+        kafkaSensorSnapshot.start();
     }
 }
