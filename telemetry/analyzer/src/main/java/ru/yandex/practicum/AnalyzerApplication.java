5374cd19ffc7690140851f286081bcc2b734f789
desidiam <desidiam@gmail.com>
2025-07-20 16:13:35 +0300

    Изменен запуск сервиса

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
@@ -3,10 +3,7 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import ru.yandex.practicum.processor.HubEventProcessor;
-import ru.yandex.practicum.processor.SnapshotProcessor;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -19,18 +16,7 @@ import java.util.concurrent.TimeUnit;
 public class AnalyzerApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
-
-        final HubEventProcessor hubEventProcessor =
-                context.getBean(HubEventProcessor.class);
-        SnapshotProcessor snapshotProcessor =
-                context.getBean(SnapshotProcessor.class);
-
-        Thread hubEventsThread = new Thread(hubEventProcessor);
-        hubEventsThread.setName("HubEventHandlerThread");
-        hubEventsThread.start();
-
-        snapshotProcessor.run();
+        SpringApplication.run(AnalyzerApplication.class, args);
     }
 
     @Bean
